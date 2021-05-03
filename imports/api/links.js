6d2d30c354e58e00e2b6916c7dd4add6fa4ec165
elsiomacedo <elsiomacedo@gmail.com>
2021-05-03 17:28:44 -0300

    LInk separado

--- imports/api/links.js
+++ imports/api/links.js
@@ -1,3 +1,30 @@
 import { Mongo } from 'meteor/mongo';
 
 export const LinksCollection = new Mongo.Collection('links');
+
+function insertLink({ title, url }) {
+    LinksCollection.insert({title, url, createdAt: new Date()});
+}
+
+export function PopuleLinks() {
+
+    insertLink({
+        title: 'Faça o Tutorial',
+        url: 'https://www.meteor.com/tutorials/react/creating-an-app'
+    });
+
+    insertLink({
+        title: 'Siga o Guia',
+        url: 'http://guide.meteor.com'
+    });
+
+    insertLink({
+        title: 'Leia a Documentação',
+        url: 'https://docs.meteor.com'
+    });
+
+    insertLink({
+        title: 'Forum de Discussões',
+        url: 'https://forums.meteor.com'
+    });
+};
--- server/main.js
+++ server/main.js
@@ -1,31 +1,9 @@
 import { Meteor } from 'meteor/meteor';
-import { LinksCollection } from '/imports/api/links';
+import { LinksCollection, PopuleLinks } from '/imports/api/links';
 
-function insertLink({ title, url }) {
-  LinksCollection.insert({title, url, createdAt: new Date()});
-}
-
-Meteor.startup(() => {
-  // If the Links collection is empty, add some data.
-  if (LinksCollection.find().count() === 0) {
-    insertLink({
-      title: 'Faça o Tutorial',
-      url: 'https://www.meteor.com/tutorials/react/creating-an-app'
-    });
-
-    insertLink({
-      title: 'Siga o Guia',
-      url: 'http://guide.meteor.com'
-    });
-
-    insertLink({
-      title: 'Leia a Documentação',
-      url: 'https://docs.meteor.com'
-    });
-
-    insertLink({
-      title: 'Forum de Discussões',
-      url: 'https://forums.meteor.com'
-    });
-  }
+Meteor.startup(() =>  {
+    // If the Links collection is empty, add some data.
+    if (LinksCollection.find().count() === 0) {
+      PopuleLinks();
+    }
 });
